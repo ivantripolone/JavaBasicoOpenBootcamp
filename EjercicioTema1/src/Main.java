f209b2c29c05e09808deb6dd282d4dcd02eb62e0
Ivan Tripolone <39201062+ivantripolone@users.noreply.github.com>
2022-07-18 01:54:33 -0300

    Resolucion Ejercicio tema 1

---
+++ EjercicioTema1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main (String [ ] args) {
+        int i = 2;
+        double d= 2.32;
+        long l= -2L;
+        boolean b= true;
+        String s= "Hola";
+
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(l);
+        System.out.println(b);
+        System.out.println(s);
+
+
+    }
+}
